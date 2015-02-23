62d512d3d135dd39de2e02f95c7fd2fa3a48c8b3
Коля <kolka.koval@gmail.com>
2015-02-23 13:52:27 +0200

    head.php

--- view/head.php
+++ view/head.php
@@ -3,9 +3,9 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600&subset=cyrillic,latin' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="/lib/css/style.css">
+        <link href="favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600&subset=cyrillic,latin' rel='stylesheet' type='text/css'/>
+        <link rel="stylesheet" type="text/css" href="/lib/css/style.css"/>
 
         <!--[if lt IE 9]>
             <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -18,7 +18,7 @@
         <?php //if Admin ?>
             <link rel="stylesheet" href="/core/lib/admin.css"/>
             <script src="/core/lib/js/script.js"></script>
-            <script src="/lib/js/admin.js"></script>
+            <script src="/core/lib/js/admin.js"></script>
             <script src="/lib/js/ckeditor/ckeditor.js"></script>
         <?php ?>
 
