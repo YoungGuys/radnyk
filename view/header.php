7ff561506c84635ea64a9c8107ebb5db2eb73fca
Коля <kolka.koval@gmail.com>
2015-03-05 13:58:13 +0200

    correct page: header ...

--- view/header.php
+++ view/header.php
@@ -1,8 +1,8 @@
 <body>
-<div class="wrap p-rel">
+<div class="wrap">
 
     <header>
-        <div class="h-top container">
+        <div class="h-top">
             <a class="mob" href="">мобільна версія</a>
             <a class="soc" href="">VK</a>
             <a class="soc" href="">FB</a>
@@ -12,14 +12,14 @@
             <span class="btn-sign-in undrln right js_mod js_mod_signIn">Вхід</span>
             <span class="btn-sign-up undrln right js_mod js_mod_signUp">Підписка</span>
         </div>
-        <div class="header p-rel">
+        <div class="header">
             <div class="container">
-                <div class="logo left">
+                <div class="logo">
                     <a href="<?= SITE ?>">
                         <img src="/lib/pic/logo.png">
                     </a>
                 </div>
-                <div class="quote p-rel left">
+                <div class="quote">
                     <?php Control::controllers(["edit"], "else", ["id" => $quote['id']]); ?>
                     <div class="q-ttl p-rel">Цитата дня</div>
                     <div class="q-cont">
@@ -29,32 +29,32 @@
                         <?=$quote['col2'];?>
                     </div>
                 </div>
-                <div class="search p-rel right">
+                <div class="search">
                     <input type="search" name="search" placeholder="П о ш у к . . ."/>
                     <input class="p-abs" type="submit" value=""/>
                 </div>
             </div>
         </div>
-        <nav class="clearfix">
-            <div class="container clearfix">
-                <ul class="clearfix">
+        <nav class="menu">
+            <div class="container">
+                <ul class="base-menu">
                     <li>
                         <a href="<?= SITE ?>News/Politics">Політика</a>
                     </li>
                     <li>
-                        <a href="<?= SITE ?>News/Economic"">Економіка</a>
+                        <a href="<?= SITE ?>News/Economic">Економіка</a>
                     </li>
                     <li>
-                        <a href="<?= SITE ?>News/Culture"">Культура</a>
+                        <a href="<?= SITE ?>News/Culture">Культура</a>
                     </li>
                     <li>
-                        <a href="<?= SITE ?>News/Sport"">Спорт</a>
+                        <a href="<?= SITE ?>News/Sport">Спорт</a>
                     </li>
                     <li>
-                        <a href="<?= SITE ?>News/World"">Світ</a>
+                        <a href="<?= SITE ?>News/World">Світ</a>
                     </li>
                 </ul>
-                <ul class="addit-menu p-rel clearfix">
+                <ul class="addit-menu">
                     <li>
                         <a href="<?= SITE ?>Photo">Фото</a>
                     </li>
