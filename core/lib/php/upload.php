220fd5552e10183ef703890c9c0d2abbf8c96292
Коля <kolka.koval@gmail.com>
2015-03-08 00:46:57 +0200

    upload files

--- core/lib/php/upload.php
+++ core/lib/php/upload.php
@@ -1,14 +1,54 @@
+<!--<?php-->
+    <!--include('/config.php');-->
+
+    <!--move_uploaded_file($_FILES['upload']['tmp_name'], "userfiles/".$name);-->
+
+    <!--$full_path = SITE . 'lib/image/ck/'.$name;-->
+
+    <!--$message = "Файл ".$_FILES['upload']['name']." загружен";-->
+    <!--$size = @getimagesize('userfiles/'.$name);-->
+
+    <!--if($size[0]<50 OR $size[1]<50){-->
+        <!--unlink('userfiles/'.$name);-->
+    <!--}-->
+<!--?>-->
+
+
 <?php
-    include('/config.php');
-
-    move_uploaded_file($_FILES['upload']['tmp_name'], "userfiles/".$name);
-
-    $full_path = SITE . 'lib/image/ck/'.$name;
-
-    $message = "Файл ".$_FILES['upload']['name']." загружен";
-    $size = @getimagesize('userfiles/'.$name);
-
-    if($size[0]<50 OR $size[1]<50){
-        unlink('userfiles/'.$name);
+    function getex($filename) {
+        return end(explode(".", $filename));
+    }
+    if($_FILES['upload'])
+    {
+        if (($_FILES['upload'] == "none") OR (empty($_FILES['upload']['name'])) )
+        {
+            $message = "Вы не выбрали файл";
+        }
+        else if ($_FILES['upload']["size"] == 0 OR $_FILES['upload']["size"] > 2050000)
+        {
+            $message = "Размер файла не соответствует нормам";
+        }
+        else if (($_FILES['upload']["type"] != "image/jpeg") AND ($_FILES['upload']["type"] != "image/jpeg") AND ($_FILES['upload']["type"] != "image/png"))
+        {
+            $message = "Допускается загрузка только картинок JPG и PNG.";
+        }
+        else if (!is_uploaded_file($_FILES['upload']["tmp_name"]))
+        {
+            $message = "Что-то пошло не так. Попытайтесь загрузить файл ещё раз.";
+        }
+    else{
+        $name =rand(1, 1000).'-'.md5($_FILES['upload']['name']).'.'.getex($_FILES['upload']['name']);
+        move_uploaded_file($_FILES['upload']['tmp_name'], "img/".$name);
+        $full_path = 'http://radnyk.com/lib/img/'.$name;
+        $message = "Файл ".$_FILES['upload']['name']." загружен";
+        $size=@getimagesize('img/'.$name);
+        if($size[0]<50 OR $size[1]<50){
+            unlink('img/'.$name);
+            $message = "Файл не является допустимым изображением";
+            $full_path="";
+        }
+    }
+        $callback = $_REQUEST['CKEditorFuncNum'];
+        echo '<script type="text/javascript">window.parent.CKEDITOR.tools.callFunction("'.$callback.'", "'.$full_path.'", "'.$message.'" );</script>';
     }
 ?>
